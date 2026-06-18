a6d193ca46053abc0106ddf73afe303ff13fdaed
Jomgzl <joseph.mogaizel@gmail.com>
2026-06-18 16:53:28 +0200

    Update index with the new JS file and made the blocks appear next to each other, added style to them and added the remove functionnality of the blocks

--- counterBlocks.js
+++ counterBlocks.js
@@ -11,12 +11,27 @@ let state = 0;
 addButton.addEventListener("click", () => {
   state++;
   number.textContent = state;
+
   let block = document.createElement("div");
 
+  block.id = "blockId";
+
+  block.style.height = "60px";
+  block.style.width = "60px";
+  block.style.backgroundColor = "navy";
+  block.style.margin = "5px";
+
+  section.style.display = "flex";
   section.append(block);
 });
 
 substractButton.addEventListener("click", () => {
-  state--;
-  number.textContent = state;
+  if (state > 0) {
+    state--;
+    number.textContent = state;
+
+    let block = document.getElementById("blockId");
+
+    block.remove();
+  }
 });
