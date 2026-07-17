7db0371cb7becc4a2ac352ee7018360f863e530a
Jomgzl <joseph.mogaizel@gmail.com>
2026-07-17 12:10:03 +0200

    Added Railway URL

--- FE/utils/handleReadBlocks.js
+++ FE/utils/handleReadBlocks.js
@@ -1,5 +1,5 @@
 export default async function handleReadBlocks() {
-  const request = await fetch("http://localhost:3000/blocks");
+  const request = await fetch("https://counter-blocks-be-production.up.railway.app/blocks");
   if (request.status === 200) {
     return await request.json();
   } else {
--- FE/utils/handleWriteBlocks.js
+++ FE/utils/handleWriteBlocks.js
@@ -1,5 +1,5 @@
 export default async function handleWriteBlocks(newBlock) {
-  const request = await fetch("http://localhost:3000/blocks", {
+  const request = await fetch("https://counter-blocks-be-production.up.railway.app/blocks", {
     method: "POST",
     body: JSON.stringify(newBlock),
     headers: {
